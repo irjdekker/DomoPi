07c23aafbe3c2b52669099b8e2bd8062446b7ae1
irjdekker <ir.j.dekker@gmail.com>
2019-05-04 14:51:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,6 +41,7 @@ STARTSTEP=1
 CONFIGFILE=$HOME/setup.conf
 SCRIPTFILE=$HOME/setup.sh
 SOURCEFILE=$HOME/source.sh
+ENCSOURCEFILE="$SOURCEFILE.enc"
 SCRIPTNAME=$0
 
 do_test_internet() {
@@ -477,9 +478,9 @@ if [ "$SCRIPTNAME" != "/home/pi/setup.sh" ] ; then
     # save script in home directory
     do_task "Save script to home directory" "wget -O $SCRIPTFILE https://raw.githubusercontent.com/irjdekker/DomoPi/master/setup.sh > $LOGFILE 2>&1"
     do_task "Change permissions on script" "chmod 700 $SCRIPTFILE > $LOGFILE 2>&1"
-    do_task "Save source file to home directory" "wget -O $SOURCEFILE.enc  https://raw.githubusercontent.com/irjdekker/DomoPi/master/source/source.sh.enc > $LOGFILE 2>&1"
-    do_task "Decrypt source file" "/usr/bin/openssl enc -aes-256-cbc -d -in $SOURCEFILE.enc -out $SOURCEFILE > $LOGFILE 2>&1"
-    do_task "Remove encrypted source file from home directory" "[ -f $SOURCEFILE.enc ] && rm -f $SOURCEFILE.enc || sleep 0.1> > $LOGFILE 2>&1"
+    do_task "Save source file to home directory" "wget -O $ENCSOURCEFILE.enc  https://raw.githubusercontent.com/irjdekker/DomoPi/master/source/source.sh.enc > $LOGFILE 2>&1"
+    do_task "Decrypt source file" "/usr/bin/openssl enc -aes-256-cbc -d -in $ENCSOURCEFILE -out $SOURCEFILE > $LOGFILE 2>&1"
+    do_task "Remove encrypted source file from home directory" "[ -f $ENCSOURCEFILE ] && rm -f $ENCSOURCEFILE || sleep 0.1> > $LOGFILE 2>&1"
     do_task "Change permissions on source file" "chmod 700 $SOURCEFILE > $LOGFILE 2>&1"
 fi
 
